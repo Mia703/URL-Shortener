fc4fe5f02043b0e8e6119605a9285dde15149537
Amya Moore <bookworm7572@gmail.com>
2023-10-09 08:06:07 -0600

    Updated base to new files
    Inserted old url shortener files

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
+# Project Description: URL Shortening Landing Page
+
+## Learning Outcomes
+
+Learn how to send HTTP requests and integrate with a thrid-party API. This project is also good to introduce you to JS libraries and/or frameworks like React or Vue. If the user refreshes the browser, try using localStorage to save the list of shortened links.
+
+## Skills Required
+
+HTML, CSS, JS, API
+
+## Features Required
+
+Users can do the following:
+
+- See the site's optimal layout based on their device's screen size -- Done
+- Reduce the length of any valid URL -- Done
+- Even after refreshing the browser, can see a list of shortened links -- Done
+- With a single click can copy the shortened link to the clipboard
+- When submitting the form, will get an error message if: The input field is currently empty
+
+## Information References
+
+- Reference the XXX.png to see the style and style requirements of this project.
+- Utilise the following API: [URL Shortener API](https://www.npmjs.com/package/@studiohyperdrive/shortener)
+- Learn how to use localStorage here: [How to use localStorage](https://blog.logrocket.com/localstorage-javascript-complete-guide/#what-localstorage-javascript)
+
+### What I learned
+
+- The importance of react "useState" to display JS variables
+- Adding 'use client' in JS files
+- Using npm node modules
+- Understanding the short version of if, else statements
+- How to prevent buttons other than the submit button to submit a form: https://stackoverflow.com/questions/932653/how-to-prevent-buttons-from-submitting-forms
+- How DIFFICULT it is to copy to clipboard: https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+- Using sessionStorage instead of localStorage, but they have the same functions: https://www.digitalocean.com/community/tutorials/js-introduction-localstorage-sessionstorage
+
+# Next.js Information
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
@@ -34,6 +72,3 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
-
-
-hello?
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,180 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import Shortener from "@studiohyperdrive/shortener";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import "./styles.css";
+
+let storage_index = 0;
+
+// counts the number of urls shortened
+function counter() {
+	storage_index += 1;
+}
+
+// if the sessionStorage length is greater than 15,
+// del contents and reset counter
+function check_storage() {
+	if (sessionStorage.length >= 16) {
+		// clear sessionStorage
+		sessionStorage.clear();
+		// set counter to zero
+		storage_index = 0;
+	}
+}
+
+// on button click, del storage and reset counter
+function clear_storage() {
+	sessionStorage.clear();
+	storage_index = 0;
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	// =========== constant variables ===========
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	// =========== shortener (= npm module that shortens urls) ===========
+	const shortener = new Shortener({
+		target: "https://shortie.com/",
+		length: 8,
+		alphabet: "alphanumeric",
+	});
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+	// =========== formik (= saves the form's data before and after submit) ===========
+	const formik = useFormik({
+		// list the values you want to save from your form
+		initialValues: {
+			url: "",
+		},
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+		/* what we'll use to validate that the form data is in the correct format
+		// Yup.string().url() may be too strict in recognising what a valid URL is,
+		// therefore you could replace .url() with the following regex:
+		// .matches( /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/, 'Enter correct url!')
+		// .required() = if the input is left blank, then "A URL is required" will show
+		// reference this: https://stackoverflow.com/questions/61634973/yup-validation-of-website-using-url-very-strict
+		*/
+		// validate the following form information:
+		validationSchema: Yup.object({
+			url: Yup.string()
+				.matches(
+					/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+					"Please enter a valid url"
+				)
+				.required("A URL is required"),
+		}),
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+		// on submission of the form, do the following:
+		// values = the data that was saved from the form
+		onSubmit: (values) => {
+			// console.log(values);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			// clear contents if sessionStorage count is over 16 and reset counter
+			check_storage();
+
+			// after submitting the form, shorten the long url and return the shortened object
+			let shortened = shortener.shorten(values.url);
+
+			// set the useState of shortened to the newly shortened url; so we can display it in HTML
+			setShortURL(shortened);
+
+			// save the shortened URL object to sessionStorage
+			sessionStorage.setItem(storage_index, JSON.stringify(shortened));
+
+			// increase counter for next entry
+			console.log("counter = " + storage_index);
+
+			// to view items in sessionStorage: console.log(JSON.parse(sessionStorage.getItem(0)))
+			// to view the object's attributes: console.log(JSON.parse(sessionStorage.getItem(0)).target)
+		},
+	});
+
+	// console.log(formik.errors);
+
+	// holds the shortened url after form submit; and used to display shortened url
+	const [shortURL, setShortURL] = useState("");
+
+	return (
+		<div id="container">
+			<div className="header-container">
+				<h1 id="header">URL Shortie</h1>
+			</div>
+			<div className="form-container">
+				<form onSubmit={formik.handleSubmit}>
+					<label className="label">
+						{/* if the input has been "touched" (i.e. clicked on) and there is an error, 
+							display the error, else display 'your long url' 
+						*/}
+						{formik.touched.url && formik.errors.url
+							? formik.errors.url
+							: "Your Long URL"}
+					</label>
+					<input
+						className="input"
+						type="url"
+						name="url"
+						value={formik.values.url}
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						placeholder="Enter your url here"
+						required
+					/>
+
+					<p className="label">Shortened URL</p>
+					<div className="input copy">
+						{/* if shortURL is not empty, then render shortened url,
+						 else render the placeholder shortie.com/ */}
+						{shortURL ? (
+							shortURL && (
+								<a id="result" href={shortURL.original} target="_blank">
+									{shortURL.target}
+								</a>
+							)
+						) : (
+							<a id="result" href="">
+								https://shortie.com/
+							</a>
+						)}
+					</div>
+
+					<div className="button-container">
+						<input
+							className="submit-btn"
+							type="submit"
+							value="Shorten Url"
+							onClick={counter}
+						/>
+
+						{/* messed up counter for some reason, but does clear sessionStorage. Why? */}
+						{/* <button className="clear-btn" type="button" onClick={onclick_check_storage}>Clear History</button> */}
+					</div>
+				</form>
+
+				<h1 id="subheader">
+					Previous URLs{" "}
+					<span className="storage-counter">
+						({sessionStorage.length <= 0 ? "0" : sessionStorage.length})
+					</span>
+				</h1>
+				<div className="previous-urls-container">
+					{/* maps through sessionStorage entries and render */}
+					{Object.entries(sessionStorage).map(([key, valueJSON]) => {
+						const value = JSON.parse(valueJSON);
+
+						return (
+							<div className="prev-entry">
+								<a
+									className="prev-url target"
+									href={value.original}
+									target="_blank"
+								>
+									{value.target}
+								</a>
+								<p className="prev-url original">{value.original}</p>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
